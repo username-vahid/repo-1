89de790e85de53df68c414a5fac7c927d90abe61
vahido <vahido@yahoo.com>
2014-01-23 11:58:05 +0330

    this is my commit msg / i changed the source

--- src/simple_git_app/Main.java
+++ src/simple_git_app/Main.java
@@ -15,7 +15,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("this is a github project");
+        System.out.println("this is a github project but it is not the last one because \n" +
+                "i canged it ha ha ha ha");
     }
 
 }
