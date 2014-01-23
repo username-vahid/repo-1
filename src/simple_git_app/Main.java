d6c45946ea5662b5c272816392056c9a804cb037
vahido <vahido@yahoo.com>
2014-01-23 11:31:42 +0330

    commit comment

---
+++ src/simple_git_app/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simple_git_app;
+
+/**
+ *
+ * @author ussocom
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("this is a github project");
+    }
+
+}
